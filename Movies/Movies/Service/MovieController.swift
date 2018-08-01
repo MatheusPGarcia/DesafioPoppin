11025b16a727febff0062cf5d54b5b5e8978aac3
Unknown <matheuspgarcia@gmail.com>
2018-08-01 17:07:51 -0300

    Adicionando classe MovieController que fara toda a lógica do service
    A criação dessa classe faz com que o código fique mais compreensivo e também de mais facil manutenção no futuro, já que encapsula em um controller toda a comunicação com o service sobre Movies

---
+++ Movies/Movies/Service/MovieController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieController.swift
+//  Movies
+//
+//  Created by Matheus Garcia on 01/08/18.
+//  Copyright © 2018 poppin 2018. All rights reserved.
+//
+
+import Foundation
+
+// This class will handle the connection between the viewController and all the Service functions
+class MovieController: NSObject {
+
+    func searchForMoviesByName(searchFor parametherValue: String, completion: @escaping (Movies) -> Void) {
+
+        let parameterKey = "s"
+
+        let formater = UrlFormatter(parameterKey: parameterKey, parameterValue: parametherValue)
+        let url = formater.getUrlToRequest()
+
+        // First make the request to get data
+        NetworkConnection.fetch(url: url) { (data) in
+
+            // With the data try to parse the json into Movie objects
+            Parser.parseMoviesSearch(forData: data, completion: { (movies) in
+                completion(movies)
+            })
+        }
+    }
+}
