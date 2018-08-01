194bb605ab399257fcbaace3d50772e4a7c25258
Unknown <matheuspgarcia@gmail.com>
2018-08-01 09:40:48 -0300

    Adicionando modelo de objeto Movie

---
+++ Movies/Movies/Model/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  Movies
+//
+//  Created by Matheus Garcia on 01/08/18.
+//  Copyright © 2018 poppin 2018. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Codable {
+
+    let title: String?
+    let year: String?
+    let imageUrl: String?
+    let imdbId: String?
+}
