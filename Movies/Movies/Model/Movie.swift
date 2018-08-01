2165999f5210bcf2375bf7b06703841890acb569
Unknown <matheuspgarcia@gmail.com>
2018-08-01 11:05:50 -0300

    Adicionando lógica do parse de objetos Movie
    Implementando custom keys para conseguir parsear as respostas da api com o uso de Decodable

--- Movies/Movies/Model/Movie.swift
+++ Movies/Movies/Model/Movie.swift
@@ -15,3 +15,32 @@ struct Movie: Codable {
     let imageUrl: String?
     let imdbId: String?
 }
+
+extension Movie {
+
+    enum JsonKeys: String, CodingKey {
+
+        //It's necessary to disable swiftlint identifier_name in this case because enum cases are formatted to match the api json keys
+        // swiftlint:disable identifier_name
+        case Title
+        case Year
+        case Poster
+        case imdbID
+        // swiftlint:enable identifier_name
+    }
+
+    // Infers custom keys to match in decode to generate Movie types
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: JsonKeys.self)
+
+        let title = try container.decode(String.self, forKey: .Title)
+        let year = try container.decode(String.self, forKey: .Year)
+        let imageUrl = try container.decode(String.self, forKey: .Poster)
+        let imdbId = try container.decode(String.self, forKey: .imdbID)
+
+        self.title = title
+        self.year = year
+        self.imageUrl = imageUrl
+        self.imdbId = imdbId
+    }
+}
---
+++ Movies/Movies/Service/Parser.swift
@@ -0,0 +1,10 @@
+//
+//  Parser.swift
+//  Movies
+//
+//  Created by Matheus Garcia on 01/08/18.
+//  Copyright © 2018 poppin 2018. All rights reserved.
+//
+
+import Foundation
+
