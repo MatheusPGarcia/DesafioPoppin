f77b27c5d9decd13ca1f0b933671f0103b1dd90c
Unknown <matheuspgarcia@gmail.com>
2018-08-01 09:55:55 -0300

    Adicionando lógica de fetch
    Utilizando uma nova classe (NetworkConnection) é possível realizar fetch para obter a data desejada

---
+++ Movies/Movies/Service/NetworkConnection.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkConnection.swift
+//  Movies
+//
+//  Created by Matheus Garcia on 01/08/18.
+//  Copyright © 2018 poppin 2018. All rights reserved.
+//
+
+import Foundation
+
+final class NetworkConnection {
+
+    // This func will be used to fetch all data
+    class func fetch(url: URL, completion: @escaping (Data) -> Void) {
+
+        let request = URLRequest(url: url)
+
+        let sessionConfiguration = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfiguration)
+
+        session.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                // TODO: Handle error in dataTask
+                print("Error fetching json: \(error)")
+                return
+            }
+
+            if let data = data {
+                completion(data)
+            }
+        }.resume()
+    }
+}
