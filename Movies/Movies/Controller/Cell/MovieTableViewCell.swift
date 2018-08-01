a6cce2753424b027b9c85ebd62398abc694c5385
Unknown <matheuspgarcia@gmail.com>
2018-08-01 17:13:31 -0300

    Adicionando tableView na MainScreen e o controller da MovieCell

---
+++ Movies/Movies/Controller/Cell/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  Movies
+//
+//  Created by Matheus Garcia on 01/08/18.
+//  Copyright © 2018 poppin 2018. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
