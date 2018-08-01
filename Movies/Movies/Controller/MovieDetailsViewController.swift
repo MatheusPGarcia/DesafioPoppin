539499cb07e4c86ebd56330908b122ca54a6f023
Unknown <matheuspgarcia@gmail.com>
2018-08-01 18:20:27 -0300

    Adicionando a storyboard e o viewController de MovieDetails

---
+++ Movies/Movies/Controller/MovieDetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MovieDetailsViewController.swift
+//  Movies
+//
+//  Created by Matheus Garcia on 01/08/18.
+//  Copyright © 2018 poppin 2018. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
