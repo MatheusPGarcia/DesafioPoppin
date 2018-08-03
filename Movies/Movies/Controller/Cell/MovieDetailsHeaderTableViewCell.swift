29e98388ae255308614468ef6c100b396c993388
Unknown <matheuspgarcia@gmail.com>
2018-08-03 13:13:07 -0300

    Adicionando tipos de Cell e seus respctivos TableViewCell controllers
    adicionando três tipos de cell:
    - Header cell (para o titulo da seção informação ou rating)
    - Info cell (que ira conter todas as informações do filme)
    - Rating cell (que ira conter todas as notas do filme)

---
+++ Movies/Movies/Controller/Cell/MovieDetailsHeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieDetailsHeaderTableViewCell.swift
+//  Movies
+//
+//  Created by Matheus Garcia on 03/08/18.
+//  Copyright © 2018 poppin 2018. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
